c8f44375a93bdc9648ed2e9e748a4471c9a3acd0
FX Soubirou <francoisxavier.soubirou@orange.com>
2024-11-07 17:51:03 +0000

    fix(readme): update README and files with copyright
    Signed-off-by: FX Soubirou <francoisxavier.soubirou@orange.com>

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,12 @@
 /*
  * Software Name : VSCode CRC: central-remote-config
- * SPDX-FileCopyrightText: Copyright (c) Orange SA
+ * SPDX-FileCopyrightText: Copyright (c) FX Soubirou - Orange SA
  * SPDX-License-Identifier: MIT
  *
  * This software is distributed under the MIT License,
  * see the "LICENSE.txt" file for more details or https://spdx.org/licenses/MIT.html
  *
- * Authors: FX Soubirou
+ * Authors: FX Soubirou - Orange SA
  * Software description: An extension to set the configuration of
  * the Continue extension from a remote server.
  */
